849fa4e4898c2a7135cf452b176acc0707388047
Maxim Mosin <me@lznet.dev>
2021-06-12 15:19:18 +0900

    Fix mistype and add logging
    Signed-off-by: Maxim Mosin <max@mosin.pw>

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# -*- cofing: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 #  BochkaBot - Main script
 #  Created by LulzLoL231 at 12/6/21
 #
 from os import environ
 from asyncio import get_event_loop
-from logging import basicConfig, INFO
+from logging import basicConfig, INFO, getLogger
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import BotCommand
@@ -20,6 +20,8 @@ basicConfig(
     format='[%(levelname)s] %(name)s (%(lineno)d) >> %(message)s',
     level=INFO
 )
+log = getLogger('BochkaBot')
+log.info('Telegram Bot BochkaBot v0.0.2')
 loop = get_event_loop()
 bot = Dispatcher(Bot(environ.get('TGBOT_TOKEN', ''), loop), loop)
 loop.run_until_complete(bot.bot.set_my_commands(cmds))
@@ -27,6 +29,7 @@ loop.run_until_complete(bot.bot.set_my_commands(cmds))
 
 @bot.message_handler(commands=['start'])
 async def start(msg: types.Message):
+    log.info(f'"start": Called by {msg.chat.mention} ({str(msg.chat.id)})')
     cnt = 'Как сделать бочку?'
     key = types.InlineKeyboardMarkup()
     key.add(types.InlineKeyboardButton(
@@ -45,6 +48,7 @@ async def query_bochka(query: types.CallbackQuery):
 
 @bot.message_handler(commands=['bochka', 'help'])
 async def bochka(msg: types.Message):
+    log.info(f'"bochka": Called by {msg.chat.mention} ({str(msg.chat.id)})')
     await msg.answer_chat_action(types.ChatActions.UPLOAD_PHOTO)
     cnt = 'Бочку сделать очень просто!'
     media = types.MediaGroup()
